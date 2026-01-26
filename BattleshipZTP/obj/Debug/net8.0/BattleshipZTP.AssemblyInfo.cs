cbf25caddabfa50325d2454feacecfe49b772410
ObjectOfClassMateusz <matitr2709@tutanota.com>
2026-01-26 20:58:00 +0100

    Empty commit

--- BattleshipZTP/obj/Debug/net8.0/BattleshipZTP.AssemblyInfo.cs
+++ BattleshipZTP/obj/Debug/net8.0/BattleshipZTP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleshipZTP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c872fa5359c0675909c9ed91c6d7a19e0aa81c06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3450d831f136735cee1649468fd4538c44f53dd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleshipZTP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleshipZTP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
